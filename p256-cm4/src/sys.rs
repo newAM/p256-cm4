9d252035c21b2cc251934a5c256979a3b389abf2
datdenkikniet <jcdra1@gmail.com>
2025-10-18 22:12:53 +0200

    Stop using P256_check_range_n directly

--- p256-cm4/src/sys.rs
+++ p256-cm4/src/sys.rs
@@ -1,3 +1,15 @@
 #[path = "./asm/mod.rs"]
 #[cfg(target_arch = "arm")]
 pub(crate) mod asm;
+
+/// Checks that the argument, as little-endian integer,
+/// is a reduced non-zero element of the scalar field.
+///
+/// In other words, that it is in the range `1..=n-1`,
+/// where `n = 2^256 - 2^224 + 2^192 - 0x4319055258e8617b0c46353d039cdaaf`.
+#[inline(always)]
+#[must_use]
+pub fn check_range_n(a: &[u32; 8]) -> bool {
+    // SAFETY: `a` is valid for the duration of the call.
+    unsafe { asm::P256_check_range_n(a) }
+}
